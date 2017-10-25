ded74238cdf6e6e346a3f4de62c1d74e01afab17
Luciano Charles de Souza <souzacomprog@gmail.com>
2017-10-25 17:36:58 -0200

    Update Model.php

--- App/Core/Model.php
+++ App/Core/Model.php
@@ -13,6 +13,7 @@ class Model {
         $option = [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8"];
         $this->db = new PDO("mysql:dbname=" . $config['dbname'] . ";host=" . $config['host'], $config['dbuser'], $config['dbpass'], $option);
         $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     }
 
 }
