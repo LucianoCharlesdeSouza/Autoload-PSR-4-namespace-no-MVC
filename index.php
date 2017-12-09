2429d3eb419b1d0dc5f28d962b73f2d76ce98080
Luciano Charles de Souza <souzacomprog@gmail.com>
2017-12-09 19:28:25 -0200

    Update index.php

--- index.php
+++ index.php
@@ -9,16 +9,6 @@ error_reporting(E_ALL);
 require './vendor/autoload.php';
 require 'config.php';
 
-//spl_autoload_register(function ($class) {
-//    if (file_exists('controllers/' . $class . '.php')) {
-//        require_once 'controllers/' . $class . '.php';
-//    } elseif (file_exists('models/' . $class . '.php')) {
-//        require_once 'models/' . $class . '.php';
-//    } elseif (file_exists('core/' . $class . '.php')) {
-//        require_once 'core/' . $class . '.php';
-//    }
-//});
-
 use App\Core\Core;
 
 $core = new App\Core\Core();
