f59f6cdf206f89a90b33c3bdda7f914c806fd67f
Luciano Charles de Souza <souzacomprog@gmail.com>
2017-12-09 19:29:56 -0200

    Update config.php

--- config.php
+++ config.php
@@ -6,18 +6,18 @@ global $config;
 $config = array();
 
 if (ENVIRONMENT == 'development') {
-    define("BASE", "http://localhost/mvc_composer_namespace/");
-    define("BASEADMIN", "http://localhost/mvc_composer_namespace/App/admin/");
-    $config['dbname'] = '';
+    define("BASE", "http://localhost/NOME_DA_PASTA_DO_PROJETO/");
+    define("BASEADMIN", "http://localhost/NOME_DA_PASTA_DO_PROJETO/App/admin/");
+    $config['dbname'] = 'dbname';
     $config['host'] = 'localhost';
     $config['dbuser'] = 'root';
-    $config['dbpass'] = '';
+    $config['dbpass'] = 'password';
 } else {
     define("BASE", "https://seudominio_real/");
     define("BASEADMIN", "https://seudominio_real/admin/");
-    $config['dbname'] = 'grupo++';
+    $config['dbname'] = 'dbname';
     $config['host'] = 'localhost';
     $config['dbuser'] = 'user';
-    $config['dbpass'] = '';
+    $config['dbpass'] = 'password';
 }
 
