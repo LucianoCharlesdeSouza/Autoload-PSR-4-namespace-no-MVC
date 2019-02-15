9af106e321d685954027cd17ba54fa836bcc8773
Luciano Charles de Souza <souzacomprog@gmail.com>
2019-02-15 13:21:15 -0200

    implementacao de rotas

--- app/Route.php
+++ app/Route.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Custom router which handles default middlewares, default exceptions and things
  * that should be happen before and after the router is initialised.
@@ -9,16 +10,16 @@ use Pecee\SimpleRouter\SimpleRouter;
 
 class Route extends SimpleRouter
 {
-    /**
-     * @throws \Exception
-     * @throws \Pecee\Http\Middleware\Exceptions\TokenMismatchException
-     * @throws \Pecee\SimpleRouter\Exceptions\HttpException
-     * @throws \Pecee\SimpleRouter\Exceptions\NotFoundHttpException
-     */
-    public static function start(): void
+	/**
+	 * @throws \Exception
+	 * @throws \Pecee\Http\Middleware\Exceptions\TokenMismatchException
+	 * @throws \Pecee\SimpleRouter\Exceptions\HttpException
+	 * @throws \Pecee\SimpleRouter\Exceptions\NotFoundHttpException
+	 */
+	public static function start() : void
 	{
 		// Load our helpers
-		require_once 'helpers.php';
+		// require_once 'helpers.php';
 		// Load our custom routes
 		require_once '../routes/web.php';
 		// Do initial stuff
